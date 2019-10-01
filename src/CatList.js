338b9443ffab8cfdb552caf8fb84e63cc8840476
Patrick Kim <pkim050@gmail.com>
2019-10-01 05:39:12 +0000

    Automatically backed up by Learn

--- src/CatList.js
+++ src/CatList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class CatList extends Component {
    renderCats = () => {
-      if (!!this.props.catPics)
+      if (this.props.catPics)
       {return this.props.catPics.map(cat => <img src={cat.url} key={cat.id} /> )}
    }
 
